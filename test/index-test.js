33ca2dacca969c253f49bf88653cf6c766f6fd27
diegonovoa@me.com <diegonovoa@me.com>
2018-05-30 18:58:19 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -24,7 +24,7 @@ describe('Fix the Scope', function() {
     })
   })
 
-  describe('funkyFunction()', function() {
+  describe('funkyFunction()()()', function() {
     it('returns a function', function() {
       expect(typeof window.funkyFunction()).toEqual('function')
     })
